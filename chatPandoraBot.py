ffd85454c68d3f394d936d26629875351fa4c4f0
Arun Balajiee Lekshmi Narayanan <ARL122@pitt.edu>
2020-01-06 14:11:22 -0500

    script and py

--- chatPandoraBot.py
+++ chatPandoraBot.py
@@ -6,15 +6,15 @@ import json
 HOST = 'api.pandorabots.com'
 USER_KEY = "0f1473bc84221a5524b0c3c6ad138e8c"
 APP_ID =  "un5f098976"
-BOT_NAME = 'cobi'
-BOTKEY = 'ydFwh9aChnKyEJ6LRrdDRdi4t8vfXSPHOMy84nRx-c7Q34HeiygUE8WcpgLuvU8rYpoe-KPxn3Y~'
-
+#BOT_NAME = 'cobi'
+#COBI_BOTKEY = 'ydFwh9aChnKyEJ6LRrdDRdi4t8vfXSPHOMy84nRx-c7Q34HeiygUE8WcpgLuvU8rYpoe-KPxn3Y~'
+nico_BOTKEY = 'ydFwh9aChnLIV1klwtqOwmj9YVoyW0StuuW1kw2rpVaOVokZNl5PZIVvneNUHKU8O_1CeaLOOCxalByRKUY_JA~~'
 
 
 
 def response(transcriptFile, pathForResponse, BOT, userid):
     # currently the sdk needs to create an instance of bot to do the talking thing
-    bot = API.Pandorabots(USER_KEY, APP_ID, HOST, BOT_NAME, BOTKEY)
+    bot = API.Pandorabots(USER_KEY, APP_ID, HOST, BOT, nico_BOTKEY)
     fileWrite = open(pathForResponse, 'w')
     with open(transcriptFile) as f:
         for line in f:
